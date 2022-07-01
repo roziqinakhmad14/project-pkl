dc573b9975248187a42ece6acd52674f8c1a601c
Akhmad Roziqin <76748239+roziqinakhmad14@users.noreply.github.com>
2022-07-01 13:08:31 +0700

    fixing layout javascript

--- CI Project PKL/project-root/app/Views/layout/page_layout.php
+++ CI Project PKL/project-root/app/Views/layout/page_layout.php
@@ -266,6 +266,9 @@ use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
     <script src="<?= base_url('assets/plugins/jqvmap/maps/jquery.vmap.usa.js')?>"></script>
     <!-- jQuery Knob Chart -->
     <script src="<?= base_url('assets/plugins/jquery-knob/jquery.knob.min.js')?>"></script>
+    <!-- InputMask -->
+    <script src="<?= base_url('assets/plugins/moment/moment.min.js'); ?>"></script>
+    <script src="<?= base_url('assets/plugins/inputmask/jquery.inputmask.min.js'); ?>"></script>
     <!-- daterangepicker -->
     <script src="<?= base_url('assets/plugins/moment/moment.min.js')?>"></script>
     <script src="<?= base_url('assets/plugins/daterangepicker/daterangepicker.js')?>"></script>
@@ -281,6 +284,65 @@ use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
     <script src="<?= base_url('assets/dist/js/demo.js')?>"></script>
     <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
     <script src="<?= base_url('assets/dist/js/pages/dashboard.js')?>"></script>
+    <script>
+        $(function () {      
+          //Datemask dd/mm/yyyy
+          $('#datemask').inputmask('dd/mm/yyyy', { 'placeholder': 'dd/mm/yyyy' })
+          //Datemask2 mm/dd/yyyy
+          $('#datemask2').inputmask('mm/dd/yyyy', { 'placeholder': 'mm/dd/yyyy' })
+          //Money Euro
+          $('[data-mask]').inputmask()
+      
+          //Date picker
+          $('#tanggal_register').datetimepicker({
+              format: 'L'
+          });
+          $('#tanggal_terbit').datetimepicker({
+              format: 'L'
+          });
+      
+          //Date and time picker
+          $('#reservationdatetime').datetimepicker({ icons: { time: 'far fa-clock' } });
+      
+          //Date range picker
+          $('#reservation').daterangepicker()
+          //Date range picker with time picker
+          $('#reservationtime').daterangepicker({
+            timePicker: true,
+            timePickerIncrement: 30,
+            locale: {
+              format: 'MM/DD/YYYY hh:mm A'
+            }
+          })
+          //Date range as a button
+          $('#daterange-btn').daterangepicker(
+            {
+              ranges   : {
+                'Today'       : [moment(), moment()],
+                'Yesterday'   : [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+                'Last 7 Days' : [moment().subtract(6, 'days'), moment()],
+                'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+                'This Month'  : [moment().startOf('month'), moment().endOf('month')],
+                'Last Month'  : [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+              },
+              startDate: moment().subtract(29, 'days'),
+              endDate  : moment()
+            },
+            function (start, end) {
+              $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'))
+            }
+          )
+      
+          //Timepicker
+          $('#timepicker').datetimepicker({
+            format: 'LT'
+          })      
+        })
+        // BS-Stepper Init
+        document.addEventListener('DOMContentLoaded', function () {
+          window.stepper = new Stepper(document.querySelector('.bs-stepper'))
+        })
+    </script>
 </body>
 
 </html>
--- CI Project PKL/project-root/app/Views/search.php
+++ CI Project PKL/project-root/app/Views/search.php
@@ -134,16 +134,16 @@
                                             </span> of 57 entries
                                         </div>
                                     </div>
-                                        <div class="col-sm-12 col-md-3">
-                                            <div class="dataTables_paginate paging_simple_numbers" id="example2_paginate">
-                                                <ul class="pagination">
-                                                    <li class="paginate_button page-item previous disabled" id="example2_previous"><a href="#" aria-controls="example2" data-dt-idx="0" tabindex="0" class="page-link">Previous</a></li>
-                                                    <li class="paginate_button page-item active"><a href="#" aria-controls="example2" data-dt-idx="1" tabindex="0" class="page-link">1</a></li>
-                                                    <li class="paginate_button page-item "><a href="#" aria-controls="example2" data-dt-idx="2" tabindex="0" class="page-link">2</a></li>
-                                                    <li class="paginate_button page-item next" id="example2_next"><a href="#" aria-controls="example2" data-dt-idx="7" tabindex="0" class="page-link">Next</a></li>
-                                                </ul>
-                                            </div>
+                                    <div class="col-sm-12 col-md-3">
+                                        <div class="dataTables_paginate paging_simple_numbers" id="example2_paginate">
+                                            <ul class="pagination">
+                                                <li class="paginate_button page-item previous disabled" id="example2_previous"><a href="#" aria-controls="example2" data-dt-idx="0" tabindex="0" class="page-link">Previous</a></li>
+                                                <li class="paginate_button page-item active"><a href="#" aria-controls="example2" data-dt-idx="1" tabindex="0" class="page-link">1</a></li>
+                                                <li class="paginate_button page-item "><a href="#" aria-controls="example2" data-dt-idx="2" tabindex="0" class="page-link">2</a></li>
+                                                <li class="paginate_button page-item next" id="example2_next"><a href="#" aria-controls="example2" data-dt-idx="7" tabindex="0" class="page-link">Next</a></li>
+                                            </ul>
                                         </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
