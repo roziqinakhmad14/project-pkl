2cda8b55eaa2725957599d37a13b02dca7a907fd
Azis Yulianas <76556157+azisyulianas@users.noreply.github.com>
2022-07-06 12:33:20 +0700

    perubahan kecil

--- project-root/app/Controllers/Search.php
+++ project-root/app/Controllers/Search.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Controllers;
-use App\Models\ReigonSelectModel;
+use App\Models\RegionSelectModel;
 use App\Models\Jenis_perizinanModel;
 use App\Models\Tabel_perizinanModel;
 
@@ -10,12 +10,12 @@ class Search extends BaseController
 {
     protected $Jenis_perizinanModel;
     protected $Tabel_perizinanModel;
-    protected $ReigonSelectModel;
+    protected $RegionSelectModel;
     public function __construct()
     {
         $this->Jenis_perizinanModel = new Jenis_perizinanModel();
         $this->Tabel_perizinanModel = new Tabel_perizinanModel();
-        $this->ReigonSelectModel = new ReigonSelectModel();
+        $this->RegionSelectModel = new RegionSelectModel();
 
     }
     public function index()
