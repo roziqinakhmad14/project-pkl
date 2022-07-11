4de71f1e0e1403ab212de51f508694f04346f078
Azis Yulianas <76556157+azisyulianas@users.noreply.github.com>
2022-07-11 11:40:47 +0700

    paginate dan validasi
    paginate sudah selesai hanya saj ada offside di front end nya
    untuk validasinya otw

--- app/Views/search.php
+++ app/Views/search.php
@@ -180,11 +180,12 @@
                                     </div>
                                     <div class="col-sm-12 col-md-3">
                                         <div class="dataTables_paginate paging_simple_numbers">
-                                            <ul class="pagination">
+                                            <?= $pager->links('dataperizinan','tabel_paging'); ?>
+                                            <!-- <ul class="pagination">
                                                 <li class="paginate_button page-item previous disabled" id="previous"><a href="#" data-dt-idx="0" tabindex="0" class="page-link">Previous</a></li>
                                                 <li class="paginate_button page-item active"><a href="#" data-dt-idx="1" tabindex="0" class="page-link">1</a></li>
                                                 <li class="paginate_button page-item next" id="next"><a href="#" data-dt-idx="7" tabindex="0" class="page-link">Next</a></li>
-                                            </ul>
+                                            </ul> -->
                                         </div>
                                     </div>
                                 </div>
