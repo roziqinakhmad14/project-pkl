c87139e4520187885f39e5e325b787a66dab31ad
Azis Yulianas <76556157+azisyulianas@users.noreply.github.com>
2022-07-14 12:46:53 +0700

    Percobaan Router

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -39,6 +39,7 @@ $routes->setAutoRoute(true);
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
+$routes->get('/Search/(:segment)/','Search::edit/$1');
 
 /*
  * --------------------------------------------------------------------
--- app/Views/search.php
+++ app/Views/search.php
@@ -130,7 +130,7 @@
                                     <td><?= $perizinan['NO_IZIN']?></td>
                                     <td><?= $perizinan['TANGGAL_TERBIT']?></td>
                                     <td><?= $perizinan['nama_perizinan']?></td>
-                                    <td class="h5" style="line-height: 20pt;"><a href="/Search/edit/<?= base64_encode($perizinan['NO_REGISTER']); ?>")>
+                                    <td class="h5" style="line-height: 20pt;"><a href="/Search/<?= base64_encode($perizinan['NO_REGISTER']); ?>")>
                                         <i class="text-hover text-primary fas fa-pen"></i></a> | <a href="/Search/delete/<?= base64_encode($perizinan['NO_REGISTER']); ?>" onclick="confirm('Apakah Anda yakin ingin menghapus data perizinan dengan No. Register <?= $perizinan['NO_REGISTER']; ?>')"><i class="text-hover text-danger  fas fa-trash"></a></i>
                                     </td>
                                 </tr>
