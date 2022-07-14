e7749d2fa9125006430113afe829fe4c724ab3f1
Sania Kamelia Hamdani <19610087@student.uin-malang.ac.id>
2022-07-14 12:17:11 +0700

    pencarian berdasarkan jenis perizinan complete

--- app/Controllers/Search.php
+++ app/Controllers/Search.php
@@ -5,6 +5,7 @@ namespace App\Controllers;
 use App\Models\RegionSelectModel;
 use App\Models\Jenis_perizinanModel;
 use App\Models\Tabel_perizinanModel;
+use LDAP\Result;
 
 class Search extends BaseController
 {
@@ -21,14 +22,13 @@ class Search extends BaseController
     {
         $izin = $this->Jenis_perizinanModel->findAll();
         $dataperizinan = $this->Tabel_perizinanModel
-        ->join('jenis_perizinan','jenis_perizinan.id_jenis_perizinan = tabel_perizinan.JENIS_PERIZINAN','LEFT')
-        ->join('kecamatan','kecamatan.id = tabel_perizinan.KECAMATAN','LEFT')
-        ->join('kelurahan','kelurahan.id = tabel_perizinan.KELURAHAN ','LEFT')
-        ->paginate(2,'dataperizinan');
+            ->join('jenis_perizinan', 'jenis_perizinan.id_jenis_perizinan = tabel_perizinan.JENIS_PERIZINAN', 'LEFT')
+            ->join('kecamatan', 'kecamatan.id = tabel_perizinan.KECAMATAN', 'LEFT')
+            ->join('kelurahan', 'kelurahan.id = tabel_perizinan.KELURAHAN ', 'LEFT')
+            ->findAll();
         $data = [
             'izin' => $izin,
-            'dataperizinan' => $dataperizinan,
-            'pager'=>$this->Tabel_perizinanModel->pager
+            'dataperizinan' => $dataperizinan
         ];
         echo view('search', $data);
     }
@@ -48,110 +48,133 @@ class Search extends BaseController
             'dataperizinan' => $dataperizinan,
             'distric' => $kecamatan
         ];
-        echo view('/edit',$data);
+        echo view('/edit', $data);
     }
     public function update($id)
     {
         // Validasi!!
-        if(!$this->validate([
-            'dateRegis'=>[
+        if (!$this->validate([
+            'dateRegis' => [
                 'rules' => 'required|is_unique[tabel_perizinan.NO_REGISTER]',
-                'errors' =>[
-                    'required'=>'NO Registrasi harus di isi',
-                    'is_unique'=>'No Registrasi sudah ada'
+                'errors' => [
+                    'required' => 'NO Registrasi harus di isi',
+                    'is_unique' => 'No Registrasi sudah ada'
                 ]
             ],
-            'dateRegis'=>[
+            'dateRegis' => [
                 'rules' => 'required',
-                'errors' =>[
-                    'required'=>'Tanggal Registrasi harus Di isi'
+                'errors' => [
+                    'required' => 'Tanggal Registrasi harus Di isi'
                 ]
             ],
-            'fullname'=>[
+            'fullname' => [
                 'rules' => 'required',
-                'errors' =>[
-                    'required'=>'Nama Lengkap harus Disi'
+                'errors' => [
+                    'required' => 'Nama Lengkap harus Disi'
                 ]
             ],
-            'address'=>[
+            'address' => [
                 'rules' => 'required',
-                'errors' =>[
-                    'required'=>'Alamat harus di isi'
+                'errors' => [
+                    'required' => 'Alamat harus di isi'
                 ]
             ],
-            'comname'=>[
+            'comname' => [
                 'rules' => 'required',
-                'errors' =>[
-                    'required'=>'Nama Perusahaan harus di isi'
+                'errors' => [
+                    'required' => 'Nama Perusahaan harus di isi'
                 ]
             ],
-            'comaddress'=>[
+            'comaddress' => [
                 'rules' => 'required',
-                'errors' =>[
-                    'required'=>'Alamat Perusahaan harus di isi'
+                'errors' => [
+                    'required' => 'Alamat Perusahaan harus di isi'
                 ]
             ],
-            'kecamatan'=>[
+            'kecamatan' => [
                 'rules' => 'required',
-                'errors' =>[
-                    'required'=>'Kecamatan harus di isi'
+                'errors' => [
+                    'required' => 'Kecamatan harus di isi'
                 ]
             ],
-            'kelurahan'=>[
+            'kelurahan' => [
                 'rules' => 'required',
-                'errors' =>[
-                    'required'=>'Alamat Perusahaan harus di isi'
+                'errors' => [
+                    'required' => 'Alamat Perusahaan harus di isi'
                 ]
             ],
-            'noIzin'=>[
+            'noIzin' => [
                 'rules' => 'required|is_unique[tabel_perizinan.NO_IZIN]',
-                'errors' =>[
-                    'required'=>'Nomer Izin harus di isi',
-                    'is_unique'=>'Nomer Izin sudah ada'
+                'errors' => [
+                    'required' => 'Nomer Izin harus di isi',
+                    'is_unique' => 'Nomer Izin sudah ada'
                 ]
             ],
-            'publishdate'=>[
+            'publishdate' => [
                 'rules' => 'required',
-                'errors' =>[
-                    'required'=>'Tanggal terbit harus di isi'
+                'errors' => [
+                    'required' => 'Tanggal terbit harus di isi'
                 ]
             ],
-            'namaIzin'=>[
+            'namaIzin' => [
                 'rules' => 'required',
-                'errors' =>[
-                    'required'=>'Jenis izin harus di isi'
+                'errors' => [
+                    'required' => 'Jenis izin harus di isi'
                 ]
             ],
-        ])){
+        ])) {
             $validation = \Config\Services::validation();
-            return redirect()->to('edit')->withInput()->with('validasi',$validation);
+            return redirect()->to('edit')->withInput()->with('validasi', $validation);
         }
         function convert($str)
         {
-            $date = explode("/",$str);
-            return $date[2].'-'.$date[0].'-'.$date[1];
+            $date = explode("/", $str);
+            return $date[2] . '-' . $date[0] . '-' . $date[1];
         }
         $this->Tabel_perizinanModel->update(base64_decode($id), [
-            'NO_REGISTER'=> $this->request->getVar('NoRegis'),
-            'TANGGAL'=> convert($this->request->getVar('dateRegis')),
-            'NAMA'=> $this->request->getVar('fullname'),
-            'ALAMAT'=> $this->request->getVar('address'),
-            'NO_HP'=> $this->request->getVar('phonenumber'),
-            'PERUSAHAAN'=> $this->request->getVar('comname'),
-            'LOKASI_USAHA'=> $this->request->getVar('comaddress'),
-            'KELURAHAN'=> $this->request->getVar('kelurahan'),
-            'KECAMATAN'=> $this->request->getVar('kecamatan'),
-            'TANGGAL_TERBIT'=> convert($this->request->getVar('publishdate')),
-            'NO_IZIN'=> $this->request->getVar('noIzin'),
-            'JENIS_PERIZINAN'=> $this->request->getVar('namaIzin')
+            'NO_REGISTER' => $this->request->getVar('NoRegis'),
+            'TANGGAL' => convert($this->request->getVar('dateRegis')),
+            'NAMA' => $this->request->getVar('fullname'),
+            'ALAMAT' => $this->request->getVar('address'),
+            'NO_HP' => $this->request->getVar('phonenumber'),
+            'PERUSAHAAN' => $this->request->getVar('comname'),
+            'LOKASI_USAHA' => $this->request->getVar('comaddress'),
+            'KELURAHAN' => $this->request->getVar('kelurahan'),
+            'KECAMATAN' => $this->request->getVar('kecamatan'),
+            'TANGGAL_TERBIT' => convert($this->request->getVar('publishdate')),
+            'NO_IZIN' => $this->request->getVar('noIzin'),
+            'JENIS_PERIZINAN' => $this->request->getVar('namaIzin')
         ]);
         return redirect()->to('/Search/index');
     }
     public function getKelurahan()
     {
         $this->RegionSelectModel = new RegionSelectModel();
-        $postData =$this->request->getPost('ID_Kecamatan');
+        $postData = $this->request->getPost('ID_Kecamatan');
         $data = $this->RegionSelectModel->getSubDistric($postData);
         echo json_encode($data);
+    }
+    public function searchByJenisPerizinan($keyword)
+    {
+        $result = $this->Tabel_perizinanModel
+            ->join('jenis_perizinan', 'jenis_perizinan.id_jenis_perizinan = tabel_perizinan.JENIS_PERIZINAN', 'LEFT')
+            ->join('kecamatan', 'kecamatan.id = tabel_perizinan.KECAMATAN', 'LEFT')
+            ->join('kelurahan', 'kelurahan.id = tabel_perizinan.KELURAHAN ', 'LEFT')
+            ->like('JENIS_PERIZINAN', $keyword)
+            ->findAll();
+
+        return $result;
+    }
+    public function search()
+    {
+        $jenisperizinan = $this->request->getVar('jenisperizinan');
+        // $jenisperizinan = 'AMDAL';
+        $dataperizinan = $this->searchByJenisPerizinan($jenisperizinan);
+        $izin = $this->Jenis_perizinanModel->findAll();
+        $data = [
+            'izin' => $izin,
+            'dataperizinan' => $dataperizinan
+        ];
+        echo view('search', $data);
     }
 }
