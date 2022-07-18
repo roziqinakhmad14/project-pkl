9ba44ded03f9e0ce5330a55c197b33578c14a591
Akhmad Roziqin <76748239+roziqinakhmad14@users.noreply.github.com>
2022-07-18 12:26:23 +0700

    Update search.php

--- app/Views/search.php
+++ app/Views/search.php
@@ -1,38 +1,38 @@
 <?= $this->extend('layout/page_layout') ?>
 <?= $this->section('sidebar_menu') ?>
-<style>
-    i.text-hover:hover {
-        opacity: 80%;
-    }
-</style>
-<nav class="mt-2">
-    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-        <!-- Add icons to the links using the .nav-icon class with font-awesome or any other icon font library -->
-        <li class="nav-item">
-            <a href="<?= site_url('Home') ?>" class="nav-link ">
-                <i class="nav-icon fas fa-tachometer-alt"></i>
-                <p>Dashboard</p>
-            </a>
-        </li>
+    <style>
+        i.text-hover:hover {
+            opacity: 80%;
+        }
+    </style>
+    <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+            <!-- Add icons to the links using the .nav-icon class with font-awesome or any other icon font library -->
+            <li class="nav-item">
+                <a href="<?= site_url('Home') ?>" class="nav-link ">
+                    <i class="nav-icon fas fa-tachometer-alt"></i>
+                    <p>Dashboard</p>
+                </a>
+            </li>
 
-        <li class="nav-item">
-            <a href="<?= site_url('Input') ?>" class="nav-link ">
-                <i class="nav-icon fas fa-edit"></i>
-                <p>
-                    Input
-                </p>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a href="<?= site_url('Search') ?>" class="nav-link active">
-                <i class="nav-icon fas fa-search"></i>
-                <p>
-                    Pencarian
-                </p>
-            </a>
-        </li>
-    </ul>
-</nav>
+            <li class="nav-item">
+                <a href="<?= site_url('Input') ?>" class="nav-link ">
+                    <i class="nav-icon fas fa-edit"></i>
+                    <p>
+                        Input
+                    </p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="<?= site_url('Search') ?>" class="nav-link active">
+                    <i class="nav-icon fas fa-search"></i>
+                    <p>
+                        Pencarian
+                    </p>
+                </a>
+            </li>
+        </ul>
+    </nav>
 <?= $this->endSection() ?>
 <?= $this->section('content') ?>
 <div class="content-wrapper">
